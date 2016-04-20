b6b078e3601114da31aea3dbda8bb7e160f54646
Tony Young <ty_training@hotmail.co.uk>
2016-04-20 17:45:30 +0100

    game can create a blank window

--- src/main/com/game/Game.java
+++ src/main/com/game/Game.java
@@ -1,5 +1,26 @@
 package main.com.game;
 
-public class Game {
+import java.awt.Canvas;
 
+public class Game extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = -2491892837841095287L;
+	
+	public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9;
+	
+	public Game() {
+		new Window(WIDTH, HEIGHT, "Create a Game", this);
+	}
+	
+	public synchronized void start() {
+		
+	}
+	
+	public void run() {
+		
+	}
+	
+	public static void main(String args[]) {
+		new Game();
+	}
 }
