0fcfde15211eb1713c55a6badcab3d760651917c
Tony Young <ty_training@hotmail.co.uk>
2016-04-26 09:45:48 +0100

    spawn enemy on certain levels, added new FastEnemy

--- src/main/com/game/Spawn.java
+++ src/main/com/game/Spawn.java
@@ -1,10 +1,13 @@
 package main.com.game;
 
+import java.util.Random;
+
 public class Spawn {
 
 	private Handler handler;
 	private HUD hud;
 	private int scoreKeeper = 0;
+	private Random rand = new Random();
 	
 	public Spawn(Handler handler, HUD hud) {
 		this.handler = handler;
@@ -18,6 +21,14 @@ public class Spawn {
 			scoreKeeper = 0;
 			
 			hud.setLevel(hud.getLevel() + 1);
+			
+			if(hud.getLevel() == 2) {
+				handler.addObject(new BasicEnemy(rand.nextInt(Game.WIDTH), rand.nextInt(Game.HEIGHT), ID.BasicEnemy, handler));
+			}else if(hud.getLevel() == 3) {
+				handler.addObject(new FastEnemy(rand.nextInt(Game.WIDTH), rand.nextInt(Game.HEIGHT), ID.FastEnemy, handler));
+			}
 		}
+		
+		
 	}
 }
