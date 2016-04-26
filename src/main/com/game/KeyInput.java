87380ad0c015e764d72003e7508b9f4454d5df7e
Tony Young <ty_training@hotmail.co.uk>
2016-04-26 10:01:15 +0100

    movement keys are more fluid

--- src/main/com/game/KeyInput.java
+++ src/main/com/game/KeyInput.java
@@ -6,9 +6,16 @@ import java.awt.event.KeyEvent;
 public class KeyInput extends KeyAdapter{
 	
 	private Handler handler;
+	private boolean[] keyDown = new boolean[4];
 	
 	public KeyInput(Handler handler) {
 		this.handler = handler;
+		
+		keyDown[0] = false;
+		keyDown[1] = false;
+		keyDown[2] = false;
+		keyDown[3] = false;
+
 	}
 	
 	public void keyPressed(KeyEvent e) {
@@ -19,10 +26,10 @@ public class KeyInput extends KeyAdapter{
 			
 			if(tempObject.getID() == ID.Player) {
 				// key events for player 1
-				if(key == KeyEvent.VK_UP) tempObject.setVelY(-5);
-				if(key == KeyEvent.VK_DOWN) tempObject.setVelY(5);
-				if(key == KeyEvent.VK_RIGHT) tempObject.setVelX(5);
-				if(key == KeyEvent.VK_LEFT) tempObject.setVelX(-5);
+				if(key == KeyEvent.VK_UP) {tempObject.setVelY(-5); keyDown[0] = true;}
+				if(key == KeyEvent.VK_DOWN) {tempObject.setVelY(5); keyDown[1] = true;}
+				if(key == KeyEvent.VK_RIGHT) {tempObject.setVelX(5); keyDown[2] = true;}
+				if(key == KeyEvent.VK_LEFT) {tempObject.setVelX(-5); keyDown[3] = true;}
 
 			}
 			//player 2
@@ -46,11 +53,15 @@ public class KeyInput extends KeyAdapter{
 			
 			if(tempObject.getID() == ID.Player) {
 				// key events for player 1
-				if(key == KeyEvent.VK_UP) tempObject.setVelY(0);
-				if(key == KeyEvent.VK_DOWN) tempObject.setVelY(0);
-				if(key == KeyEvent.VK_RIGHT) tempObject.setVelX(0);
-				if(key == KeyEvent.VK_LEFT) tempObject.setVelX(0);
+				if(key == KeyEvent.VK_UP) keyDown[0] = false;
+				if(key == KeyEvent.VK_DOWN) keyDown[1] = false;
+				if(key == KeyEvent.VK_RIGHT) keyDown[2] = false;
+				if(key == KeyEvent.VK_LEFT) keyDown[3] = false;
 
+				//vertical movement
+				if(!keyDown[0] && !keyDown[1]) tempObject.setVelY(0);
+				//horizontal movement
+				if(!keyDown[2] && !keyDown[3]) tempObject.setVelX(0);
 			}
 			//player 2
 //			if(tempObject.getID() == ID.Player2) {
